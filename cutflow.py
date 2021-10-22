4d9886da97c5d8705013e5822e662fb986e4333a
Giacomo Ubaldi <giacomoubaldi@gmail.com>
2021-10-22 11:51:54 +0200

    add of config file

--- cutflow.py
+++ cutflow.py
@@ -205,25 +205,6 @@ class cutFlow:
 
 
 
-inFileName = "bkg2_Demo.root"
-
-nameTree = ["multiboson_NoSys","multiboson_NoSys"]
-
-cuts = [
-          ("Preselection 1 lepton, $E_\mathrm{T}^\mathrm{miss} > $ 150 GeV, $N_\mathrm{jet30} = $ 2-3, $m_{\mathrm{T}}$ $>$ 50 GeV", "trigMatch_metTrig&&met>150&&nJet30>=2&&nJet30<4&&nLep_base==1&&nLep_signal==1&&mt>50"),
-          ("$N_{\mathrm{b-jets,30}} =$ 2", "nBJet30_DL1==2"),
-          ("$m_{\mathrm{bb}}$ $>$ 50 GeV", "mbb>50"),
-          ("105 $<$ $m_{\mathrm{bb}}$ $<$ 135 GeV", "mbb>105&&mbb<135"),
-          ("$m_{\mathrm{CT}}$ $>$ 160 GeV", "mct>160"),
-          ("$E_\mathrm{T}^\mathrm{miss} > $ 200 GeV", "met>200"),
-           #("met>200", "met > 200"),
-        ]
-
-
-cuts2 = []
-
-outFileName = "prova.txt"
-
 
 
 outFile = open (outFileName, 'w')
@@ -248,13 +229,3 @@ sig.GetCounts()
 sig.SetSNR(bkg)
 sig.GetSNR(bkg)
 
-"""
-print ("init")
-tree = ["d"]
-print(tree)
-
-SetTree(inFileName, numTrees, nameTree, tree)
-print ("outside func")
-print (tree)
-
-"""
