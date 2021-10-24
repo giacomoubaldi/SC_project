f222ae1b28c456296db1f1575567ed103e211869
Giacomo Ubaldi <giacomoubaldi@gmail.com>
2021-10-24 18:01:08 +0200

    add new config syntax

---
+++ config_Demo.py
@@ -0,0 +1,30 @@
+{
+
+
+        #file .root for the signal and the branches i am interested in
+        "inFileName_sig"    : "signal_Demo.root",
+        "nameTree_sig"      : ["C1N2_WZ_300_0_NoSys"],
+
+        #file .root for the background and the branches i am interested in
+        "inFileName_bkg"    : "bkg2_Demo.root",
+        "nameTree_bkg"      : ["multiboson_NoSys"],
+
+        #common cuts for the cutflow
+        "cuts"              : [
+          ("Preselection 1 lepton, $E_\mathrm{T}^\mathrm{miss} > $ 150 GeV, $N_\mathrm{jet30} = $ 2-3, $m_{\mathrm{T}}$ $>$ 50 GeV", "trigMatch_metTrig&&met>150&&nJet30>=2&&nJet30<4&&nLep_base==1&&nLep_signal==1&&mt>50"),
+          ("$N_{\mathrm{b-jets,30}} =$ 2", "nBJet30_DL1==2"),
+          ("$m_{\mathrm{bb}}$ $>$ 50 GeV", "mbb>50"),
+          ("105 $<$ $m_{\mathrm{bb}}$ $<$ 135 GeV", "mbb>105&&mbb<135"),
+          ("$m_{\mathrm{CT}}$ $>$ 160 GeV", "mct>160"),
+          ("$E_\mathrm{T}^\mathrm{miss} > $ 200 GeV", "met>200"),
+           #("met>200", "met > 200"),
+        ],
+
+        #common weight for data
+        "weight"            : "genWeight*eventWeight*pileupWeight*leptonWeight*bTagWeight",
+
+        #common outfile for the read out
+        "outFileName"       : "provaw.txt"
+
+
+}
